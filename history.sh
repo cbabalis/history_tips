c087dd2c7f67fc4e26e610ad1ea8724696bd7a51
Babis Babalis <babisbabalis@gmail.com>
2017-03-04 16:32:43 +0200

    basic commands

--- history.sh
+++ history.sh
@@ -1,3 +1,7 @@
 # Remove git@ for submodule update
 vim .gitmodules
 vim .git/config
+# copy only the .html files that are NOT already in the destination folder
+cp -u *.html destination
+ln -s source dest
+
