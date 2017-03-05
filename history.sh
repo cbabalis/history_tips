a8431d32264e0008db29b4f4f8c9aa9e745cf75a
Babis Babalis <babisbabalis@gmail.com>
2017-03-05 17:20:57 +0200

    some more commands

--- history.sh
+++ history.sh
@@ -4,4 +4,6 @@ vim .git/config
 # copy only the .html files that are NOT already in the destination folder
 cp -u *.html destination
 ln -s source dest
-
+type ls
+whatis ls
+alias testprep='cd ~/workspace/SDL_TestAutomation; http=; https=; HTTP=; HTTPS=; export python'
